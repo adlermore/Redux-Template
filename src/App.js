67ec91d3bd350e55e0de6313bafcf543c5362ce8
adlermore <armorbita@gmail.com>
2023-10-14 18:07:10 +0400

    Redux Template Created!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-import Counter from './Counter';
-
+// import Counter from './Counter';
+import Counter from './features/counter/Counter'
 
 function App() {
   return (
     <div className="App">
-
       <Counter />      
-    </div>
+    </div>  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import {createStore } from 'redux';
+
+import store from './app/store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
-const mainReducer = (state , acton )=>{
-  console.log(state, acton);
-}
-
-const store = createStore(mainReducer);
 
 root.render(
 
@@ -21,5 +15,4 @@ root.render(
       <App />
     </Provider>
   </React.StrictMode>
-
 );
