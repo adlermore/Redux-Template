15d41ab6210406e49322fef31ea3946a9310b561
adlermore <armorbita@gmail.com>
2023-10-15 19:10:21 +0400

    new commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/CounterSlice'
+import { configureStore , applyMiddleware } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/CounterSlice';
+import loggerMiddleware from 'redux-logger';
 
+
+const middlewares = applyMiddleware(loggerMiddleware);
 
 export default configureStore({
     reducer: {
